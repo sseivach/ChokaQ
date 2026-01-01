707a4f3a65836368f6ac496e5c173d5325026aa8
Sergei Seivach <sseyvach@gmail.com>
2026-01-01 01:26:16 -0500

    Feat: add test run

--- samples/ChokaQ.SampleApp/Program.cs
+++ samples/ChokaQ.SampleApp/Program.cs
@@ -1,26 +1,39 @@
+using ChokaQ.Abstractions;
+using ChokaQ.Core.Queues;
+using ChokaQ.Core.Workers;
+using ChokaQ.Core.Jobs;
+using ChokaQ.Core.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+// Register the Queue as Singleton (The "Pipe")
+builder.Services.AddSingleton<InMemoryQueue>();
+// Alias for the interface
+builder.Services.AddSingleton<IChokaQQueue>(sp => sp.GetRequiredService<InMemoryQueue>());
+
+// Register the Background Worker
+builder.Services.AddHostedService<JobWorker>();
+
+// Register the Handler for our test job
+builder.Services.AddTransient<IChokaQJobHandler<PrintMessageJob>, PrintMessageJobHandler>();
+
+// ==========================================
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.MapGet("/", () => "ChokaQ Sample App is running! Use POST /enqueue to test.");
+
+// 2. TEST ENDPOINT
+// We inject the queue interface and push a job manually.
+app.MapPost("/enqueue", async (IChokaQQueue queue, [FromBody] string text) =>
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    var job = new PrintMessageJob(text);
 
-app.UseHttpsRedirection();
+    // Fire and forget!
+    await queue.EnqueueAsync(job);
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+    return Results.Accepted(value: new { Status = "Enqueued", Payload = text });
+});
 
 app.Run();
