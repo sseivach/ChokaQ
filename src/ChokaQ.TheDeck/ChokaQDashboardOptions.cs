fff5469a6ab55f0a13f9cabe893cf4916e6d19fb
Sergei Seivach <sseyvach@gmail.com>
2026-01-31 15:08:05 -0500

    feat: adding theDeck

---
+++ src/ChokaQ.TheDeck/ChokaQDashboardOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ChokaQ.Dashboard;
+
+public class ChokaQDashboardOptions
+{
+    /// <summary>
+    /// The relative URL path where the dashboard will be mapped.
+    /// Default: "/chokaqdeck"
+    /// </summary>
+    public string RoutePrefix { get; set; } = "/chokaqdeck";
+}
